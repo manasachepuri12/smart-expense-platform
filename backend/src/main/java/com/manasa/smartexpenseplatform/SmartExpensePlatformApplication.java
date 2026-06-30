3ed1ce69e3f1affc590ff9485b9297295564c2a8
Chepuri Manasa <manasa12.chepuri@gmail.com>
2026-06-30 16:49:43 +0530

    Initial Spring Boot backend setup

---
+++ backend/src/main/java/com/manasa/smartexpenseplatform/SmartExpensePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.manasa.smartexpenseplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartExpensePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartExpensePlatformApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/manasa/smartexpenseplatform/SmartExpensePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manasa.smartexpenseplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartExpensePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
